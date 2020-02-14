4f4522a23977e7c57eabd4ba3b59932d1cd69008
Spencer William Barnes <interstellarjumper@gmail.com>
2020-02-14 14:40:45 -0600

    added servo calibration code

--- arduinoCode/robot230/robot230.h
+++ arduinoCode/robot230/robot230.h
@@ -26,12 +26,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 // Motor Shield Setup:
 // AFMS: Motorshield default address is 0x60, but we have some in lab that are 0x61
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x60);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(4);
 
 // This will be iterated through to press buttons. Each number corresponds to a servo
-String ordering = "0110";
+// String ordering = "0110";
+String ordering = "0123456789";
 // String ordering = "314159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378";
 
 // Our degree constants
