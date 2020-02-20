a3024824ec697f1717c66d77f079a6f467224565
Spencer Barnes <interstellarjumper@gmail.com>
2020-02-20 15:51:33 -0600

    Initial restructuring of robot230

---
+++ arduinoCode/robot230/robot230Minimum/robot230Minimum.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Adafruit_MotorShield.h>
+
+enum State{
+    start, 
+    getToWall,
+    dropWings,
+    pushButtons,
+    end
+};
+
+double degreesToPwm(int degree);
+
+void calibrateButtons();
+
+void moveWheels(int myDelayTime, int myDirection);
+
+void pressButton(int servoNumber);
+
+void resetState();
+
+void startState();
+
+void getToWallState();
+
+void dropWingsState();
+
+void pushButtonsState();
+
+void endState();
+
+// Servo shield setup: called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// You can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+
+// Motor Shield Setup:
+// AFMS: Motorshield default address is 0x60, but we have some in lab that are 0x61
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x60);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(4);
+
+int targetSpeed = 200;
+
+// This will be iterated through to press buttons. Each number corresponds to a servo
+// String ordering = "0110";
+String ordering = "0123456789";
+// String ordering = "314159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378";
+
+// Our degree constants
+int calibrationPos = 90; //Set to 90 when attaching the arm
+int restingPosLeft = calibrationPos - 10;
+int restingPosRight = calibrationPos + 10;
+int pressingPosLeft = calibrationPos + 25;
+int pressingPosRight = calibrationPos - 25;
---
+++ arduinoCode/robot230/robot230Minimum/robot230Minimum.ino
@@ -0,0 +1,219 @@
+#include "robot230.h"
+
+State state = start;
+
+// Converte degrees to PWM for servo sheild
+double restingValueLeft = degreesToPwm(restingPosLeft);
+double restingValueRight = degreesToPwm(restingPosRight);
+double pressingValueLeft = degreesToPwm(pressingPosLeft);
+double pressingValueRight = degreesToPwm(pressingPosRight);
+double calibratingValue = degreesToPwm(calibrationPos);
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("16 channel PWM test!");
+    Serial.println("Input values 1-4095");
+
+    // Servo setup
+    pwm.begin();
+    pwm.setPWMFreq(100);
+
+    // Motor setup
+    AFMS.begin();
+
+    leftMotor->setSpeed(0);
+    leftMotor->run(FORWARD);
+    leftMotor->run(RELEASE);
+
+    rightMotor->setSpeed(0);
+    rightMotor->run(FORWARD);
+    rightMotor->run(RELEASE);
+
+    leftMotor->setSpeed(targetSpeed);
+    rightMotor->setSpeed(targetSpeed);
+
+    // Limit switch setup
+    pinMode(12, INPUT_PULLUP);
+}
+
+boolean reset = false;
+int positionInPi = 0;
+
+void loop()
+{
+    // Serial.println("State: ");
+    // Serial.println(state);
+
+    switch (state)
+    {
+    case start:
+        startState();
+        break;
+
+    case dropWings:
+        dropWingsState();
+        // TODO: Change calibrate buttons to put buttons in pre press state
+        calibrateButtons();
+        break;
+
+    case getToWall:
+        getToWallState();
+        break;
+
+    case pushButtons:
+        pushButtonsState();
+        break;
+
+    case end:
+        endState();
+        break;
+
+    default:
+        break;
+    }
+}
+
+void calibrateButtons()
+{
+    for (int i = 0; i < 10; i++)
+        pwm.setPWM(i, 0, calibratingValue); 
+}
+
+void moveWheels(int myDelayTime, int myDirection)
+{
+    // Turn the motors on
+    leftMotor->run(myDirection);
+    rightMotor->run(myDirection);
+
+    Serial.println("forward");
+
+    // TODO: changed to elapsed time
+    // Go for .... ms
+    delay(myDelayTime);
+
+    // Turn the motors off
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+}
+
+void pressButton(int servoNumber)
+{
+    int direction = 0;
+    if (servoNumber == 0 || servoNumber == 1 || servoNumber == 3 || servoNumber == 4 || servoNumber == 7 || servoNumber == 15)
+        direction = 1;
+    else
+        direction = 0;
+
+    // TODO: change to elapsed time
+    if (direction) // Buttons 0 through 4 and buttons 5 through 9 are oriented in two different directions
+    {
+        pwm.setPWM(servoNumber, 0, pressingValueLeft);
+        delay(250);
+        pwm.setPWM(servoNumber, 0, restingValueLeft);
+    }
+    else
+    {
+        pwm.setPWM(servoNumber, 0, pressingValueRight);
+        delay(250);
+        pwm.setPWM(servoNumber, 0, restingValueRight);
+    }
+}
+
+double degreesToPwm(int degree)
+{
+    return (725 / 180 * degree + 275);
+}
+
+void resetState()
+{
+    for (int i = 0; i < 15; i++)
+    {
+
+        if (i == 2)
+            pwm.setPWM(i, 0, pressingValueRight);
+        else if (i == 7)
+            pwm.setPWM(i, 0, pressingValueLeft);
+        else if (1 > 12)
+            pwm.setPWM(i, 0, calibratingValue);
+        // TODO: we aren't using servos 10 - 12, should we skip them in this loop?
+        
+    }
+
+    reset = true;
+}
+
+void startState()
+{
+    if (digitalRead(12))
+    {
+        while (digitalRead(12))
+        {
+            Serial.println("Waiting for RELEASE");
+        }
+
+        state = dropWings;
+    }
+    return;
+}
+
+void getToWallState()
+{
+    // leftMotor->run(FORWARD);
+    // rightMotor->run(FORWARD);
+
+    while (!digitalRead(12))
+    {
+        Serial.println("Getting to the wall");
+
+        leftMotor -> run(FORWARD);
+        rightMotor -> run(FORWARD);
+    }
+        
+    state = pushButtons;
+    return;
+}
+                                             
+void dropWingsState()
+{
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+
+    pwm.setPWM(14, 0, degreesToPwm(120));
+    pwm.setPWM(15, 0, degreesToPwm(50));
+    delay(500);
+
+    pwm.setPWM(14, 0, calibratingValue);
+    pwm.setPWM(15, 0, calibratingValue);
+
+    state = getToWall;
+    return;
+}
+
+void pushButtonsState()
+{
+    leftMotor->run(RELEASE);
+    rightMotor->run(RELEASE);
+
+    String charOfOrdering = ordering.substring(positionInPi, positionInPi + 1);
+    pressButton(charOfOrdering.toInt());
+
+    // TODO: change to elapsed time?
+    delay(1000);
+
+    if (positionInPi >= ordering.length())
+        state = end;
+    else if (!digitalRead(12))
+        state = getToWall;
+
+    positionInPi++;
+    return;
+}
+
+void endState()
+{
+    if (!reset)
+        resetState();
+
+    return;
+}
